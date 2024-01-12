8dd70685d24029ca8dadaf2bdf9e0afd076a51ee
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-01-12 17:39:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 //complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(newAge) {
+    this._age = newAge;
+  }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+  get age() {
+    return this._age;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// Example usage:
+
+// Creating an instance of the Person class
+const person = new Person("John", 25);
+console.log(`Person: ${person.name}, Age: ${person.age}`);
+
+// Creating an instance of the Student class
+const student = new Student("Alice", 20);
+student.study();
+
+// Creating an instance of the Teacher class
+const teacher = new Teacher("Mr. Smith", 35);
+teacher.teach();
