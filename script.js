ed0170a4fdb9aa91293523efd493e3b73d933657
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-01-20 22:54:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 //complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
-	get name(){
-		return this.name;
-	}
-	set age(newAge){
-		this.age=newAge;
-	}
-	get age(){
-		return this.age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(newAge) {
+    if (newAge >= 0) {
+      this._age = newAge;
+    } else {
+      console.error("Age cannot be negative.");
+    }
+  }
 }
 
 class Student extends Person {
-	study() {
+  study() {
     console.log(`${this.name} is studying`);
   }
 }
 
 class Teacher extends Person {
-	 teach() {
+  teach() {
     console.log(`${this.name} is teaching`);
   }
 }
